639211c7417deb1fad54295fca1cbdf1ec985337
BOThiago <thiago.barreto.oliveira22@gmail.com>
2024-08-07 00:46:20 -0300

    first commit

---
+++ src/main/java/com/example/ToDoList/ToDoListApplication.java
@@ -0,0 +1,12 @@
+package com.example.ToDoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/ToDoList/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
