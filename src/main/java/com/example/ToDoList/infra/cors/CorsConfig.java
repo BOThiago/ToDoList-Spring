39dffd625be56c01eccaaf52d6a1f86f82aedd90
BOThiago <thiago.barreto.oliveira22@gmail.com>
2024-08-07 00:52:32 -0300

    feat: enable cors

---
+++ src/main/java/com/example/ToDoList/infra/cors/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.example.ToDoList.infra.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @SuppressWarnings("null")
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "DELETE", "PUT");
+    }
+}
